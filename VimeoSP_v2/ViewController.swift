ca00e5a66308f349b426a557a474a4e7683dde1b
Willy <willy548798@gmail.com>
2025-11-16 13:40:36 +0800

    build: Vimeo Side Project v2

--- VimeoSP_v2/ViewController.swift
+++ VimeoSP_v2/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Vimeo Wordmark_Black")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupLogo()
+    }
+
+    private func setupLogo() {
+        view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(240)
+        }
     }
 
 
