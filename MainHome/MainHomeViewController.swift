d529f8538b906c11e62e640f7d65ce642615b481
Willy <willy548798@gmail.com>
2025-11-30 22:40:22 +0800

    feat: 新增首頁 logo 與 搜尋按鈕

--- MainHome/MainHomeViewController.swift
+++ MainHome/MainHomeViewController.swift
@@ -11,8 +11,53 @@ import SnapKit
 
 class MainHomeViewController: BaseMainViewController{
     
+    private enum LogoConfig {
+        static let maxWidth: CGFloat = 100
+        static let height: CGFloat = 24
+        static let imageName = "Vimeo Wordmark_White"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Home"
+        setupNavBar()
+    }
+    
+    private func setupNavBar() {
+        setupLogoTitle()
+        setupSearchButton()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    private func setupLogoTitle() {
+        let logoImageView = UIImageView(image: UIImage(named: LogoConfig.imageName))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.clipsToBounds = true
+        
+        let containerView = UIView()
+        containerView.addSubview(logoImageView)
+        logoImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.lessThanOrEqualTo(LogoConfig.maxWidth)
+            make.height.equalTo(LogoConfig.height)
+        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: containerView)
+    }
+    
+    private func setupSearchButton() {
+        let config = UIImage.SymbolConfiguration(weight: .semibold)
+        let searchImage = UIImage(systemName: "magnifyingglass", withConfiguration: config)
+        
+        let searchButton = UIBarButtonItem(
+            image: searchImage,
+            style: .plain,
+            target: self,
+            action: #selector(searchButtonTapped)
+        )
+        searchButton.tintColor = .vimeoWhite
+        navigationItem.rightBarButtonItem = searchButton
+    }
+    
+    @objc private func searchButtonTapped() {
+        print("Search button tapped")
     }
 }
